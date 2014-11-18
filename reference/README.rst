3df3ffba602975940f9837bc7b2853906f21d71d
Ward Vandewege <ward@curoverse.com>
2014-11-18 10:51:49 -0500

    Update setup.py so that package version numbering is more flexible.
    Rename README.md to README.rst so that it also renders on pypi
    
    No issue #

---
+++ reference/README.rst
@@ -0,0 +1,35 @@
+==================================================================
+Common workflow language tool description reference implementation
+==================================================================
+
+This is intended to be a lightweight reference implementation of the common
+workflow language tool description.
+
+Install
+-------
+
+From source::
+
+  git clone https://github.com/curoverse/common-workflow-language.git
+  cd common-workflow-language/reference
+  easy_install .
+
+With pip::
+
+  pip install cwltool
+
+
+Run on the command line
+-----------------------
+
+  ``cwltool [tool] [job]``
+
+Use as a library
+----------------
+
+Add::
+
+  import cwltool
+
+to your script.
+
--- reference/setup.py
+++ reference/setup.py
@@ -1,8 +1,31 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+import os
+import subprocess
+import time
+
+from setuptools import setup, find_packages
+
+SETUP_DIR = os.path.dirname(__file__)
+README = os.path.join(SETUP_DIR, 'README.rst')
+
+cmd_opts = {'egg_info': {}}
+try:
+    git_tags = subprocess.check_output(
+        ['git', 'log', '--first-parent', '--max-count=1',
+         '--format=format:%ct %h', SETUP_DIR]).split()
+    assert len(git_tags) == 2
+except (AssertionError, OSError, subprocess.CalledProcessError):
+    pass
+else:
+    git_tags[0] = time.strftime('%Y%m%d%H%M%S', time.gmtime(int(git_tags[0])))
+    cmd_opts['egg_info']['tag_build'] = '.{}.{}'.format(*git_tags)
+
 
 setup(name='cwltool',
-      version='1.0',
+      version='0.1',
       description='Common workflow language reference implementation',
+      long_description=open(README).read(),
       author='Common workflow language working group',
       author_email='common-workflow-language@googlegroups.com',
       url="https://github.com/curoverse/common-workflow-language",
@@ -19,5 +42,6 @@ setup(name='cwltool',
       tests_require=[],
       entry_points={
           'console_scripts': [ "cwltool=cwltool.main:main" ]
-      }
+      },
+      options=cmd_opts,
 )
