d5f78ed78b3c61f3053406ae2fae0921df5e7483
Nebojsa Tijanic <nebojsa.tijanic@sbgenomics.com>
2015-01-29 17:01:38 +0100

    line count wf

---
+++ examples/to_string.py
@@ -0,0 +1,14 @@
+import json
+
+
+def main():
+    with open('job.cwl.json') as inp, open('result.cwl.json', 'w') as out:
+        job = json.load(inp)
+        with open(job['inputs']['file1']['path']) as fp:
+            contents = fp.read()
+        json.dump({
+            'result': contents
+        }, out)
+
+if __name__ == '__main__':
+    main()
