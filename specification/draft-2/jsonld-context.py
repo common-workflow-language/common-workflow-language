753e21807d856089a5c49cf4f29c959b89b92779
Peter Amstutz <peter.amstutz@curoverse.com>
2015-04-10 10:43:34 -0400

    Generate schema as well as context.

--- specification/draft-2/jsonld-context.py
+++ specification/draft-2/jsonld-context.py
@@ -9,6 +9,10 @@ import copy
 import pprint
 import re
 import sys
+import rdflib
+from rdflib import Graph
+import rdflib.namespace
+from rdflib.namespace import RDF, RDFS
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 cwl_avsc = os.path.join(module_dir, '../../schemas/draft-2/cwl-avro.yml')
@@ -20,6 +24,20 @@ context = {
     "dct": "http://purl.org/dc/terms/",
     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 }
+
+g = Graph()
+
+namespaces = {
+    "cwl": rdflib.namespace.Namespace('http://github.com/common-workflow-language#'),
+    "avro": rdflib.namespace.Namespace("http://github.com/common-workflow-language#avro/"),
+    "wfdesc": rdflib.namespace.Namespace("http://purl.org/wf4ever/wfdesc#"),
+    "dct": rdflib.namespace.DCTERMS,
+    "rdf": rdflib.namespace.RDF,
+    "rdfs": rdflib.namespace.RDFS
+}
+
+for k,v in namespaces.items():
+    g.bind(k, v)
 
 def pred(datatype, field, name):
     v = None
@@ -38,22 +56,46 @@ def pred(datatype, field, name):
         else:
             context[name] = v
 
+    return v
+
 with open(cwl_avsc) as f:
     j = yaml.load(f)
-    for t in j:
-        if t["type"] == "record":
-            if t["name"] in context:
-                raise Exception("Collision on %s" % t["name"])
 
-            if "jsonldPrefix" in t:
-                context[t["name"]] = "%s:%s" % (t["jsonldPrefix"], t["name"])
+for t in j:
+    if t["type"] == "record":
+        classnode = namespaces["cwl"][t["name"]]
+        g.add((classnode, RDF.type, RDFS.Class))
+
+        if t["name"] in context:
+            raise Exception("Collision on %s" % t["name"])
+
+        if "jsonldPrefix" in t:
+            context[t["name"]] = "%s:%s" % (t["jsonldPrefix"], t["name"])
+        else:
+            context[t["name"]] = "cwl:%s" % (t["name"])
+
+        for i in t["fields"]:
+            v = pred(t, i, i["name"])
+
+            if isinstance(v, basestring):
+                v = v if v[0] != "@" else None
             else:
-                context[t["name"]] = "cwl:%s" % (t["name"])
+                v = v["@id"] if v["@id"][0] != "@" else None
 
-            for i in t["fields"]:
-                pred(t, i, i["name"])
-        elif t["type"] == "enum":
-            for i in t["symbols"]:
-                pred(t, None, i)
+            if v:
+                (ns, ln) = rdflib.namespace.split_uri(unicode(v))
+                propnode = namespaces[ns[0:-1]][ln]
+                g.add((propnode, RDF.type, RDF.Property))
+                g.add((propnode, RDFS.domain, classnode))
+
+                # TODO generate range from datatype.
+
+        if "extends" in t:
+            g.add((classnode, RDFS.subClassOf, namespaces["cwl"][t["extends"]]))
+    elif t["type"] == "enum":
+        for i in t["symbols"]:
+            pred(t, None, i)
 
 print json.dumps(context, indent=4, sort_keys=True)
+
+print g.serialize(format='turtle')
