612b38347770849951f223840af152268edcf859
Peter Amstutz <peter.amstutz@curoverse.com>
2015-05-12 09:08:55 -0400

    Updating to load on "id" instead of draft-1 "$ref"

--- reference/cwltool/ref_resolver.py
+++ reference/cwltool/ref_resolver.py
@@ -35,11 +35,12 @@ class Loader(object):
 
     def load(self, url, base_url=None):
         base_url = base_url or 'file://%s/' % os.path.abspath('.')
-        return self.resolve_ref({'$ref': url}, base_url)
+        return self.resolve_ref({'id': url}, base_url)
 
     def resolve_ref(self, obj, base_url):
-        ref, mixin, checksum = obj.pop('$ref', None), obj.pop('$mixin', None), obj.pop('$checksum', None)
-        ref = ref or mixin
+        ref, mixin = obj.pop('id', None)
+        if ref[0] == "#":
+            return obj
         url = urlparse.urljoin(base_url, ref)
         if url in self.resolved:
             return self.resolved[url]
@@ -48,11 +49,9 @@ class Loader(object):
         self.resolving[url] = True
         doc_url, pointer = urlparse.urldefrag(url)
         document = self.fetch(doc_url)
-        fragment = copy.deepcopy(resolve_pointer(document, pointer))
+        fragment = copy.deepcopy(resolve_fragment(document, pointer))
         try:
-            self.verify_checksum(checksum, fragment)
-            if isinstance(fragment, dict) and mixin:
-                fragment = dict(obj, **fragment)
+            fragment = dict(obj, **fragment)
             result = self.resolve_all(fragment, doc_url)
         finally:
             del self.resolving[url]
@@ -62,7 +61,7 @@ class Loader(object):
         if isinstance(document, list):
             iterator = enumerate(document)
         elif isinstance(document, dict):
-            if '$ref' in document or '$mixin' in document:
+            if 'id' in document:
                 return self.resolve_ref(document, base_url)
             iterator = document.iteritems()
         else:
@@ -95,24 +94,24 @@ class Loader(object):
         self.fetched[url] = result
         return result
 
-    def verify_checksum(self, checksum, document):
-        if not checksum:
-            return
-        hash_method, hexdigest = checksum.split('$')
-        if hexdigest != self.checksum(document, hash_method):
-            raise RuntimeError('Checksum does not match: %s' % checksum)
-
-    def checksum(self, document, method='sha1'):
-        if method not in ('md5', 'sha1'):
-            raise NotImplementedError('Unsupported hash method: %s' % method)
-        normalized = json.dumps(document, sort_keys=True, separators=(',', ':'))
-        return getattr(hashlib, method)(normalized).hexdigest
-
-
 POINTER_DEFAULT = object()
 
+def resolve_fragment(document, frag):
+    if isinstance(document, dict):
+        if document.get("id") == frag:
+            return document
+        for d in document:
+            r = resolve_fragment(document[d], frag)
+            if r:
+                return r
+    elif isinstance(document, list):
+        for d in document:
+            r = resolve_fragment(d, frag)
+            if r:
+                return r
+    return None
 
-def resolve_pointer(document, pointer, default=POINTER_DEFAULT):
+def resolve_json_pointer(document, pointer, default=POINTER_DEFAULT):
     parts = urlparse.unquote(pointer.lstrip('/#')).split('/') \
         if pointer else []
     for part in parts:
@@ -130,16 +129,7 @@ def resolve_pointer(document, pointer, default=POINTER_DEFAULT):
                 raise ValueError('Unresolvable JSON pointer: %r' % pointer)
     return document
 
-
 loader = Loader()
-
-
-def to_json(obj, fp=None):
-    default = lambda o: (o.__json__() if callable(getattr(o, '__json__', None))
-                         else str(o))
-    kwargs = dict(default=default, indent=2, sort_keys=True)
-    return json.dump(obj, fp, **kwargs) if fp else json.dumps(obj, **kwargs)
-
 
 def from_url(url, base_url=None):
     return loader.load(url, base_url)
