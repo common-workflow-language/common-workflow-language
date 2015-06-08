2fb87ea0f64f9d88be7cd8ccd0bda95d0a90a81d
Peter Amstutz <peter.amstutz@curoverse.com>
2015-06-08 16:01:57 -0400

    Cranking through spec updates.

--- reference/cwltool/avro_ld/makedoc.py
+++ reference/cwltool/avro_ld/makedoc.py
@@ -242,6 +242,11 @@ def avrold_doc(j, outdoc):
     <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    """)
+
+    outdoc.write("<title>%s</title>" % (j[0]["name"]))
+
+    outdoc.write("""
     <style>
     html {
       height:100%;
