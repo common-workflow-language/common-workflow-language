16e8f78d1423e18165126500b2c3359f0de76155
Peter Amstutz <peter.amstutz@curoverse.com>
2016-07-07 12:38:44 -0400

    re-import schema salad

--- v1.0/salad/schema_salad/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-import sys
-import typing
-
-__author__ = 'peter.amstutz@curoverse.com'
-
-_logger = logging.getLogger("salad")
-_logger.addHandler(logging.StreamHandler())
-_logger.setLevel(logging.INFO)
--- v1.0/salad/schema_salad/__main__.py
+++
@@ -1,5 +0,0 @@
-from . import main
-import sys
-import typing
-
-sys.exit(main.main())
