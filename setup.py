449b5b8941a332d4355ec45f1df097bed560783c
Peter Amstutz <peter.amstutz@curoverse.com>
2016-08-10 15:46:55 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ install_requires.append("avro")  # TODO: remove me once cwltool is
 extras_require = {}               # TODO: to be removed when the above is added
 
 setup(name='schema-salad',
-      version='1.15',
+      version='1.16',
       description='Schema Annotations for Linked Avro Data (SALAD)',
       long_description=open(README).read(),
       author='Common workflow language working group',
