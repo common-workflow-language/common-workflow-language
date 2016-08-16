6df505ca5aa0886030f7e33c20ecaafa55dadfa1
Michael R. Crusoe <michael.crusoe@gmail.com>
2016-08-16 17:41:21 +0300

    clarify module != package name

--- release-test.sh
+++ release-test.sh
@@ -3,9 +3,10 @@
 set -e
 set -x
 
-proj=schema-salad
+package=schema-salad
+module=schema_salad
 repo=https://github.com/common-workflow-language/schema_salad.git
-run_tests="py.test ${proj}"
+run_tests="py.test --pyarg ${module}"
 
 rm -Rf testenv? || /bin/true
 
@@ -19,11 +20,11 @@ virtualenv testenv4
 source testenv1/bin/activate
 make install-dependencies
 make test
-pip uninstall -y ${proj} || true; pip uninstall -y ${proj} || true; make install
-mkdir testenv1/not-${proj}
-# if there is a subdir named '${proj}' py.test will execute tests
+pip uninstall -y ${package} || true; pip uninstall -y ${package} || true; make install
+mkdir testenv1/not-${module}
+# if there is a subdir named '${module}' py.test will execute tests
 # there instead of the installed module's tests
-pushd testenv1/not-${proj}; ../bin/${run_tests}; popd
+pushd testenv1/not-${module}; ../bin/${run_tests}; popd
 
 
 # Secondly we test via pip
@@ -31,13 +32,13 @@ pushd testenv1/not-${proj}; ../bin/${run_tests}; popd
 cd testenv2
 source bin/activate
 pip install -U setuptools==3.4.1
-pip install -e git+${repo}@${HEAD}#egg=${proj}
-cd src/${proj}
+pip install -e git+${repo}@${HEAD}#egg=${package}
+cd src/${package}
 make install-dependencies
 make dist
 make test
-cp dist/${proj}*tar.gz ../../../testenv3/
-pip uninstall -y ${proj} || true; pip uninstall -y ${proj} || true; make install
+cp dist/${package}*tar.gz ../../../testenv3/
+pip uninstall -y ${package} || true; pip uninstall -y ${package} || true; make install
 cd ../.. # no subdir named ${proj} here, safe for py.testing the installed module
 bin/${run_tests}
 
@@ -47,12 +48,12 @@ bin/${run_tests}
 cd ../testenv3/
 source bin/activate
 pip install -U setuptools==3.4.1
-pip install ${proj}*tar.gz
+pip install ${package}*tar.gz
 pip install nose
-tar xzf ${proj}*tar.gz
-cd ${proj}*
+tar xzf ${package}*tar.gz
+cd ${package}*
 make dist
 make test
-pip uninstall -y ${proj} || true; pip uninstall -y ${proj} || true; make install
-mkdir ../not-${proj}
-pushd ../not-${proj} ; ../bin/${run_tests}; popd
+pip uninstall -y ${package} || true; pip uninstall -y ${package} || true; make install
+mkdir ../not-${module}
+pushd ../not-${module} ; ../bin/${run_tests}; popd
